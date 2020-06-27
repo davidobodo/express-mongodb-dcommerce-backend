430ca8c57ada7958a001ba190a7bf79a172c9351
David Obodo <obododavid5@gmail.com>
2020-06-27 05:47:54 +0100

    test: check if husky for prettier is working

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express')
+const express = require("express")
 
 const app = express()
 
-app.listen(5000, () => console.log('app running on localhost 5000'))
+app.listen(5000, () => console.log("app running on localhost 5000"))
