a991d34d6a18c33acb4af14315e807b7b45a0ccf
David Obodo <obododavid5@gmail.com>
2020-06-27 14:31:25 +0100

    feat: hash password

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,49 +1,60 @@
 const { uuid } = require("uuidv4");
 const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
 
 const signup = async (req, res, next) => {
-    const result = validationResult(req);
+    const { firstName, lastName, email, password } = req.body;
 
+    //------------------------------------------------------------------------
+    //Express validation checker
+    //------------------------------------------------------------------------
+    const result = validationResult(req);
     if (!result.isEmpty()) {
         return next(new HttpError(result.errors[0].msg, 422));
     }
 
-    const { firstName, lastName, email, password } = req.body;
-
-    const createduser = new User({
-        firstName,
-        lastName,
-        email,
-        password,
-    });
-
     //------------------------------------------------------------------------
-    //check if a user with the same email exists before signing up
+    //Check if a user with the same email exists before signing up
     //------------------------------------------------------------------------
     let existingUser;
-
     try {
         existingUser = await User.findOne({ email: email });
     } catch (err) {
         return next(new HttpError("Signing up failed, please try again", 500));
     }
-
     if (existingUser) {
-        console.log(existingUser);
+        console.log(existingUser, "existing user");
         return next(
             new HttpError("User exists already, please login instead", 422)
         );
     }
 
     //------------------------------------------------------------------------
-    //create user on database
+    //Hash users password before storing in database
+    //------------------------------------------------------------------------
+    let hashedPassword;
+    try {
+        hashedPassword = await bcrypt.hash(password, 12);
+    } catch (err) {
+        return next(new HttpError("Signing up failed, please try again", 500));
+    }
+
+    const createdUser = new User({
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+    });
+
+    //------------------------------------------------------------------------
+    //Create user on database
     //------------------------------------------------------------------------
     try {
-        const result = await createduser.save();
-        console.log(result);
+        const result = await createdUser.save();
+        console.log(result, "result");
     } catch (err) {
         return next(new HttpError("Signing up failed, please try again", 500));
     }
