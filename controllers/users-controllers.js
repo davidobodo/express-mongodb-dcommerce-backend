9356482f6c0f07ca1275decd77fa88146229eaec
David Obodo <obododavid5@gmail.com>
2020-06-27 06:46:55 +0100

    feat: add user controller to test signup endpoint

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,3 +1,16 @@
-const signup = async (req, res, next) => {};
+const { uuid } = require("uuidv4");
+
+const signup = async (req, res, next) => {
+    const { name, email, password } = req.body;
+
+    const createUser = {
+        id: uuid(),
+        name,
+        email,
+        password,
+    };
+
+    res.status(201).json({ user: createUser });
+};
 
 exports.signup = signup;
