fb67df750cc85cbbb5dd44136f5df11b66567436
David Obodo <obododavid5@gmail.com>
2020-06-27 06:24:14 +0100

    feat: create route and controller for users

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require("express")
-const bodyParser = require("body-parser")
+const express = require("express");
+const bodyParser = require("body-parser");
+const usersRoutes = require("./routes/users-routes");
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use("/api/users", usersRoutes);
 
-app.listen(5000, () => console.log("app running on localhost 5000"))
+app.listen(5000, () => console.log("app running on localhost 5000"));
