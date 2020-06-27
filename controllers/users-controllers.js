3754153e74ce564ee2892f0b2232b3df162bbc9a
David Obodo <obododavid5@gmail.com>
2020-06-27 12:13:03 +0100

    feat: validate signup form payload

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,6 +1,13 @@
 const { uuid } = require("uuidv4");
+const { validationResult } = require("express-validator");
+
+const HttpError = require("../models/http-error");
 
 const signup = async (req, res, next) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+        return next(new HttpError(result.errors[0].msg, 422));
+    }
     const { name, email, password } = req.body;
 
     const createUser = {
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,7 +1,21 @@
 const express = require("express");
+const { check } = require("express-validator");
 const usersControllers = require("../controllers/users-controllers");
 
 const router = express.Router();
 
-router.post("/signup", usersControllers.signup);
+router.post(
+    "/signup",
+    [
+        check("name").notEmpty().withMessage("name should not be empty"),
+        check("email")
+            .normalizeEmail()
+            .isEmail()
+            .withMessage("email is invalid"),
+        check("password")
+            .isLength({ min: 6 })
+            .withMessage("must be at least 6 chars long"),
+    ],
+    usersControllers.signup
+);
 module.exports = router;
